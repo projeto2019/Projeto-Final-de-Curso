6339badb1e4ac36d12204c6c109ac6de4ac71ce8
Francisco Reis <frkikoreis@gmail.com>
2019-04-05 12:22:04 +0100

    Update asc712.ino
    Codigo para 5V.

--- asc712/asc712.ino
+++ asc712/asc712.ino
@@ -1,5 +1,5 @@
 #define Vin A0
-const float Offset = 327;
+const float Offset = 510; //327
 
 void setup(){ 
  Serial.begin(9600);
@@ -8,11 +8,10 @@ void setup(){
 void loop(){
  
  float voltage = (5.0 / 1023.0)* (analogRead(Vin)- Offset);
- float current = voltage / 0.044; //(0.66 * (3.3/5));
- //Serial.println(analogRead(Vin));
+ float current = voltage / 0.066; //(0.66 * (3.3/5));
+ 
+ Serial.print("Current: ");
  Serial.println(current);
  delay(1000);
 
- 
- 
 }
